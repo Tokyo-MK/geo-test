b8fcdd9340d995ae0f8d612a9212cb8e85b2e7a1
kentakenta99 <kentakenta99@gmail.com>
2026-02-25 06:08:07 -0800

    Simplify API response in test.js

--- api/test.js
+++ api/test.js
@@ -1,8 +1,3 @@
 export default function handler(req, res) {
-  const key = process.env.ANTHROPIC_API_KEY;
-  res.status(200).json({
-    hasKey: !!key,
-    keyStart: key ? key.slice(0, 12) : 'なし'
-  });
+  res.status(200).json({ ok: true });
 }
-```
